0a3513ee533c3c36484dae858faf3a23699fe4a9
AssetGrav <417051@mail.ru>
2022-10-19 21:53:16 +0600

    useEffect problem

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useEffect } from 'react'
 import './App.css';
+import Header from './components/Header/Header';
 import { useTelegram } from './hook/useTelegram';
 
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
-
+  
   useEffect(() => {
     tg.ready()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
   return (
     <div className="App">
-     Work
+      <Header />
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
