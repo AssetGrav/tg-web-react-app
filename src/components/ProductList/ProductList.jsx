a80222c7206e223e9e32789138541343fd0d1293
AssetGrav <417051@mail.ru>
2022-10-20 23:27:11 +0600

    queryId add

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import ProductItem from '../ProductItem/ProductItem'
 import './ProductList.css'
 import { useTelegram } from '../../hook/useTelegram';
@@ -22,7 +22,31 @@ const getTotalPrice = (items = []) => {
 
 function ProductList() {
   const [addedItems, setAddedItems] = useState([])
-  const {tg} = useTelegram()
+  const {tg, queryId} = useTelegram()
+
+  const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+        fetch('https://localhost:8000', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data)
+        })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [onSendData])
 
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find(item => item.id === product.id)
